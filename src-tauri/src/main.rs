8bf3134e3e50a690f9cc8b547fe032f624229462
Jhoan Borges <jhoan.borges@hexagun.mx>
2026-03-04 12:14:39 -0600

    fixed blank issue

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+  // Force software rendering to work around NVIDIA KMS/DRM permission issues on Linux
+  std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+  std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+  std::env::set_var("LIBGL_ALWAYS_SOFTWARE", "1");
+
   app_lib::run();
 }
